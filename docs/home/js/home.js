d7b72fe23b4705fe9be06afe47ff553f10776193
Veldymanov <veldymanov@gmail.com>
2017-10-12 10:58:28 +0300

    footer finished, build docs

--- docs/home/js/home.js
+++ docs/home/js/home.js
@@ -1,1 +1,1 @@
-"use strict";jQuery(document).ready(function(){$("p a").css({"background-color":"#ccc"})}),"serviceWorker"in navigator&&navigator.serviceWorker.register("service-worker.js").then(function(e){return console.log("Service Worker registration succeeded. Scope is "+e.scope)}).catch(function(e){return console.log("Registration failed"+e)});
+"use strict";jQuery(document).ready(function(){$("p a").css({"background-color":"#ccc"})});
