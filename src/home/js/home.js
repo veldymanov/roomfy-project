92c50956c6bdc4ea6bb0d951326591ecbe1f960d
veldymanov <veldymanov@gmail.com>
2017-10-12 07:22:35 +0300

    home commit

--- src/home/js/home.js
+++ src/home/js/home.js
@@ -1,12 +1,3 @@
 jQuery(document).ready(function(){
     $('p a').css({'background-color': '#ccc'});
 });
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-//Register Service Worker
-////////////////////////////////////////////////////////////////////////////////////////////////
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('service-worker.js')
-		.then( reg => console.log('Service Worker registration succeeded. Scope is ' + reg.scope) )
-		.catch( error => console.log('Registration failed' + error) )
-}
